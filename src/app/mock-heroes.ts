bc0d067362fb74b2efb434cdce726469914bfc56
unknown <sdacanay97@gmail.com>
2023-09-02 15:19:36 +0800

    angularTuts

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: ' Dr. quak quak' },
+  { id: 13, name: ' Bat Man' },
+  { id: 14, name: ' Super Man' },
+  { id: 15, name: ' Flash Light' },
+  { id: 16, name: ' Wonder Women' },
+  { id: 17, name: ' Joker' },
+  { id: 18, name: ' Wolverine' },
+  { id: 19, name: ' Magneto' },
+  { id: 20, name: ' Spider Man' }
+];
